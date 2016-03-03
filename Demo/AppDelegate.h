9600dc2f3905840f249d21dd5e4b0ce8a82a58d8
apple <apple@appledeair.lan>
2016-03-03 16:57:13 +0800

    Initial Commit

---
+++ Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by apple on 16/3/3.
+//  Copyright © 2016年 TianMing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
